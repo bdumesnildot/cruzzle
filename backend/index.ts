626c3e4ffe41a70bd6bfbf99c157b0286a124a9f
bdumesnildot <benoit.du-mesnildot@jdc.fr>
2024-05-23 09:16:59 +0200

    fix dockerfiles and add dockerignore

--- backend/index.ts
+++ backend/index.ts
@@ -4,8 +4,8 @@ import app from "./src/app";
 
 dotenv.config(<DotenvConfigOptions>{ silent: true });
 
-const port: number = parseInt(process.env.APP_PORT ?? "8000", 10);
+const port: number = parseInt(process.env.API_PORT ?? "8000", 10);
 
 app.listen(port, () => {
-  console.info(`⚡️[server]: Server is running at http://localhost:${port}`);
+  console.info(`⚡️[server]: Server is running on port: ${port}`);
 });
